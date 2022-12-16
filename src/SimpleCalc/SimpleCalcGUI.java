b1458f61e29d1c9f1ff7bf968057ab0d713174a7
Elijah Fernandez <fjelijah215@gmail.com>
2022-12-16 18:10:28 +0800

    Renamed, initialized, created main

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,10 +1,38 @@
 package SimpleCalc;
 
-import javax.swing.*;
+import LeapYearChecker.LeapYearGUI;
 
-public class SimpleCalcGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
+    private JTextField tfNumber1;
+    private JComboBox cbOperations;
+    private JButton btnCompute;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String getFirstInt = tfNumber1.getText();
+                String getSecondInt = tfNumber2.getText();
+                String operator = (String) cbOperations.getSelectedItem();
+
+                int parseFirstInt = (Integer.parseInt(getFirstInt));
+                int parseSecondInt = (Integer.parseInt(getSecondInt));
+            }
+        });
+    }
+    public static void main(String[] args) {
+        SimpleCalcGUI app = new SimpleCalcGUI();
+        app.setTitle("Simple Calculator");
+        app.setContentPane(app.panel1);
+        app.setSize(600, 300);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
